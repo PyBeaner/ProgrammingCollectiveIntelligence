0ed56000a10c7995fa26367f793782ec9cfa58a5
PyBeaner <758046738@qq.com>
2015-06-06 00:53:56 +0800

    pydelicious

--- Character2/delicicousrec.py
+++ Character2/delicicousrec.py
@@ -1,1 +1,10 @@
-__author__ = 'PyBeaner'
+from pydelicious import get_popular,get_userposts,get_urlposts
+
+def initializeUserDict(tag,count=5):
+    user_dict = {}
+    for p1 in get_popular(tag=tag)[0:count]:
+        for p2 in get_urlposts(p1["href"]):
+            user = p2['user']
+            user_dict[user]  ={}
+
+    return user_dict
