53ab7c4c504a300e67f5f17ca87a62881226038b
PyBeaner <758046738@qq.com>
2015-06-07 14:22:46 +0800

    clawler

---
+++ Chapter4/searchengine.py
@@ -0,0 +1,46 @@
+class crawler:
+    # Initialize the crawler with the name of database
+    def __init__(self,dbname):
+        pass
+
+    def __del__(self):
+        pass
+
+    def dbcommit(self):
+        pass
+
+    # Auxilliary function for getting an entry id and adding
+    # it if it's not present
+    def getentryid(self,table,field,value,createnew=True):
+        return None
+
+    # Index an individual page
+    def addtoindex(self,url,soup):
+        print("Indexing %s" % url)
+
+    # Extract the text from an HTML page (no tags)
+    def gettextonly(self,soup):
+        return None
+
+    # Separate the words by any non-whitespace character
+    def separatewords(self,text):
+        return None
+
+    # return whether a url is indexed
+    def isindexed(self,url):
+        return False
+
+    # add a link between two pages
+    def addlinkref(self,urlFrom,urlTo,linkText):
+        pass
+
+    # Starting with a list of pages, do a breadth
+    # first search to the given depth, indexing pages
+    # as we go
+    def crawl(self,pages,depth=2):
+        pass
+
+    # Create the database tables
+    def createindextables(self):
+        pass
+    
