f5f9e95c1ed6368f7a93270467f40b9d8d758769
PyBeaner <758046738@qq.com>
2015-06-07 14:11:14 +0800

    print cluster

--- Chapter3/clusters.py
+++ Chapter3/clusters.py
@@ -1,5 +1,13 @@
 from math import sqrt
 
+class bicluster:
+    def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
+        self.left = left
+        self.right = right
+        self.vec = vec
+        self.id = id
+        self.distance = distance
+
 def pearson(v1,v2):
     sum1 = sum(v1)
     sum2 = sum(v2)
@@ -14,6 +22,39 @@ def pearson(v1,v2):
     if den==0:return 0
     return 1.0-num/den
 
+def hcluster(rows,distance=pearson):
+    distances = {}
+    currentclusterid = -1
+    clust = [bicluster(rows[i],id=i) for i in range(len(rows))]
+    while len(clust)>1:
+        lowestpair = (0,1)
+        closest = distance(clust[0].vec,clust[1].vec)
+
+        for i in range(len(clust)):
+            for j in range(i+1,len(clust)):
+                if (clust[i].id,clust[j].id) not in distances:
+                   distances[(clust[i].id,clust[j].id)] = distance(clust[i].vec,clust[j].vec)
+
+                d = distances[(clust[i].id,clust[j].id)]
+                if(d<closest):
+                    closest = d
+                    lowestpair = (i,j)
+
+        mergevec = [
+            (clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2.0
+            for i in range(len(clust[0].vec))
+        ]
+
+        newcluster = bicluster(mergevec,left=clust[lowestpair[0]],
+                               right=clust[lowestpair[1]],
+                               distance=closest,id=currentclusterid)
+
+        currentclusterid -= 1
+        del clust[lowestpair[1]]
+        del clust[lowestpair[0]]
+        clust.append(newcluster)
+    return clust[0]
+
 def readfile(filename):
     with open(filename) as file:
         lines = file.readlines()
@@ -26,3 +67,22 @@ def readfile(filename):
             rownames.append(p[0])
             data.append([float(x) for x in p[1:]])
         return rownames,colnames,data
+
+def printclust(clust,labels=None,n=0):
+    # indent to make a hierarchy layout
+    for i in range(n): print(' '),
+    if clust.id<0:
+    # negative id means that this is branch
+        print('-')
+    else:
+        # positive id means that this is an endpoint
+        if labels==None: print(clust.id)
+        else: print(labels[clust.id])
+# now print the right and left branches
+    if clust.left!=None: printclust(clust.left,labels=labels,n=n+1)
+    if clust.right!=None: printclust(clust.right,labels=labels,n=n+1)
+
+if(__name__ == "__main__"):
+    blognames,words,data = readfile("blogdata.txt")
+    clust = hcluster(data)
+    printclust(clust,labels=blognames)
