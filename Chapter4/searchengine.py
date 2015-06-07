88a4320da24258ac5da826fa1cc115eb451ba016
PyBeaner <758046738@qq.com>
2015-06-07 14:55:09 +0800

    crawl pages

--- Chapter4/searchengine.py
+++ Chapter4/searchengine.py
@@ -1,3 +1,9 @@
+from urllib import request
+from bs4 import *
+
+ignorewords = {'the',"of","to","and","a","in","is","it"}
+
+
 class crawler:
     # Initialize the crawler with the name of database
     def __init__(self,dbname):
@@ -38,9 +44,37 @@ class crawler:
     # first search to the given depth, indexing pages
     # as we go
     def crawl(self,pages,depth=2):
-        pass
+        for i in range(depth):
+            newpages = set()
+            for page in pages:
+                try:
+                    c = request.urlopen(page)
+                except Exception as e:
+                    print("Could not open %s" % page)
+                    print("Due to:"+e.__str__())
+                    continue
+                soup = BeautifulSoup(c.readall())
+                self.addtoindex(page,soup)
+
+                links = soup("a")
+                for link in links:
+                    if "href" in dict(link.attrs):
+                        url = request.urljoin(page,link["href"])
+                        if url.find("'")!=-1:continue
+                        url = url.split("#")[0]# remove url portion
+                        if url[0:4]=="http" and not self.isindexed(url):
+                            newpages.add(url)
+
+                        linkText = self.gettextonly(link)
+                        self.addlinkref(page,url,linkText)
+                self.dbcommit()
+            pages = newpages
 
     # Create the database tables
     def createindextables(self):
         pass
-    
+
+if(__name__=="__main__"):
+    pagelist = ["http://www.baidu.com"]
+    c = crawler("")
+    c.crawl(pagelist)
