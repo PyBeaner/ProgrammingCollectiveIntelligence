c1a517211c98845d5022f5fd557a2882f474a017
XiaomingSu <758046738@qq.com>
2015-06-09 13:10:53 +0800

    calculate cost of a schedule

--- Chapter5/optimization.py
+++ Chapter5/optimization.py
@@ -25,7 +25,6 @@ for line in open("schedule.txt").readlines():
 
 def getminutes(t):
     x = time.strptime(t,"%H:%M")
-    print(x[3],x[4])# hour:minute
     return x[3]*60+x[4]
 
 # a list numbers represent their choices
@@ -39,6 +38,39 @@ def printschedule(r):
         ret = flights[(destination,origin)][r[d+1]]
         print("%10s%10s %5s-%5s $%3s %5s-%5s $%3s" % (name,origin,out[0],out[1],out[2], ret[0],ret[1],ret[2]))
 
+def schedulecost(sol):
+    totalprice = 0
+    latestarrival = 0
+    earliestdep = 24*60
+    totalwait = 0
+
+    for d in range(int(len(sol)/2)):
+        origin = people[d][1]
+        outbound = flights[(origin,destination)][sol[2*d]]
+        returnf = flights[(destination,origin)][sol[2*d+1]]
+
+        # total cost(in and out)
+        totalprice += outbound[2]+returnf[2]
+        arrivial = getminutes(outbound[1])
+        if arrivial>latestarrival:latestarrival=arrivial
+        depart = getminutes(returnf[0])
+        if depart<earliestdep:earliestdep=depart
+
+    for d in range(int(len(sol)/2)):
+        origin = people[d][1]
+        outbound = flights[(origin,destination)][sol[2*d]]
+        returnf = flights[(destination,origin)][sol[2*d+1]]
+
+        totalwait+=latestarrival-getminutes(outbound[1])
+        totalwait+=getminutes(returnf[0])-earliestdep
+
+    # extra day of car rental(50)
+    if latestarrival>earliestdep:totalprice+=50
+    return totalprice
+
+
 if __name__ == "__main__":
     s = [1,4,3,2,7,3,6,3,2,4,5,3]
     printschedule(s)
+    cost = schedulecost(s)
+    print(cost)
