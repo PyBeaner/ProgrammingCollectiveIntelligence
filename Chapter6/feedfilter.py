f0eebb4f9bd2789226f065d25edf14afbef59252
XiaomingSu <758046738@qq.com>
2015-06-11 20:27:31 +0800

    add feature to the classifier

--- Chapter6/feedfilter.py
+++ Chapter6/feedfilter.py
@@ -20,6 +20,32 @@ def read(feed,classifier):
         cat = input("Enter category: ")
         classifier.train(fulltext,cat)
 
+def entryfeatures(entry):
+    splitter = re.compile("\\W*")
+    f = {}
+    # Extract the title words and annotate
+    titlewords = [s.lower() for s in splitter.split(entry["title"])
+                  if len(s)>2 and len(s)<20]
+    for w in titlewords:f["Title:"+w] = 1
+    summarywords = [s.lower() for s in splitter.split(entry['summary'])
+                  if len(s)>2 and len(s)<20]
+    uc = 0
+    for i in range(len(summarywords)):
+        w = summarywords[i]
+        f[w] = 1
+        if w.isupper():uc+=1
+
+        if i<len(summarywords)-1:
+            twoword = " ".join(summarywords[i:i+1])
+            f[twoword] = 1
+
+    # Keep creator and publisher whole
+    f['Publisher:'+entry['publisher']]=1
+    # UPPERCASE is a virtual word flagging too much shouting
+    if float(uc)/len(summarywords)>0.3: f['UPPERCASE']=1
+    return f
+
+
 if __name__ == "__main__":
     c = fisherclassifier(getwords)
     c.setdb("python_feed.db")
