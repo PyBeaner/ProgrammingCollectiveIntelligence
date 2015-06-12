96674176653e3c4e62c13d887325108f27259706
XiaomingSu <758046738@qq.com>
2015-06-12 16:42:59 +0800

    draw the tree with pillow

--- Chapter7/treepredict.py
+++ Chapter7/treepredict.py
@@ -1,3 +1,5 @@
+from PIL import Image,ImageDraw
+
 __author__ = 'PyBeaner'
 
 my_data = [
@@ -130,6 +132,36 @@ def getdepth(tree):
     return max(getdepth(tree.tb),getdepth(tree.fb)) + 1
 
 
+def drawnode(draw, tree, x, y):
+    if tree.results:
+        txt = " \n".join(["%s:%d"%v for v in tree.results.items()])
+        draw.text((x-20,y),txt,(0,0,0))
+        return
+    # get the width of each branch
+    w1 = getwidth(tree.fb)*100
+    w2 = getwidth(tree.tb)*100
+    # determine the total space required by this node
+    left = x-(w1+w2)/2
+    right = x+(w1+w2)/2
+
+    # draw the condition string
+    draw.text((x-20,y-10),str(tree.col)+":"+str(tree.value),(0,0,0))
+    # draw links to the branches
+    draw.line((x,y,left+w1/2,y+100), fill=(255,0,0))
+    draw.line((x,y,right-w2/2,y+100),fill=(255,0,0))
+    # Draw the branch nodes
+    drawnode(draw,tree.fb,left+w1/2,y+100)
+    drawnode(draw,tree.tb,right-w2/2,y+100)
+
+
+def drawtree(tree,jpeg="tree.jpg"):
+    w = getwidth(tree)*100
+    h = getdepth(tree)*100+120
+    img = Image.new("RGB",(w,h),(255,255,255))
+    draw = ImageDraw.Draw(img)
+    drawnode(draw,tree,w/2,20)
+    img.save(jpeg,"jpeg")
+
 if __name__ == "__main__":
     g = giniimpurity(my_data)
     print(g)
@@ -144,3 +176,5 @@ if __name__ == "__main__":
 
     tree = buildtree(my_data,entropy)
     printtree(tree)
+
+    drawtree(tree,"tree.jpg")
